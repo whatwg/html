cb8445520f10e044d23cfd3ff7fc38e064edf1f1
Ian Hickson <ian@hixie.ch>
2015-08-26 17:37:31 -0400

    Update to be source material for the build

---
+++ link-fixup.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', function () {
+  if (window.location.hash.length < 1)
+    return;
+
+  var fragid = window.location.hash.substr(1);
+  if (fragid && document.getElementById(fragid))
+    return;
+
+  var script = document.createElement('script');
+  script.src = 'fragment-links.js';
+  document.body.appendChild(script);
+});
