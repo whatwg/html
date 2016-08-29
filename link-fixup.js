76a4fc2655799bb50bfcb1ea0039ccf94384187e
Takeshi Kurosawa <taken.spc@gmail.com>
2016-08-30 04:19:56 +0900

    Rewrite link-fixup.js
    This makes various changes to the link-fixup.js code:
    
    - Integrates with https://github.com/whatwg/wattsi/pull/25 and
      https://github.com/whatwg/html-build/pull/88 in order to let the build
      process produce a separate fragment-links.json file for consumption
      by external tools.
    - Inlines the fragment links into the link-fixup.js file (via the build
      system) in order to start redirects as soon as possible.
    - Uses <script defer> instead of waiting for DOMContentLoaded in order
      to simplify the logic.
    - Moves the redirection logic that Wattsi was writing into
      fragment-links.js into link-fixup.js (see
      https://github.com/whatwg/wattsi/blob/1854204f23e082d5bac4382d833e20f7a61560f6/src/wattsi.pas#L1566-L1580).

--- link-fixup.js
+++ link-fixup.js
@@ -1,12 +1,30 @@
-window.addEventListener('DOMContentLoaded', function () {
-  if (window.location.hash.length < 1)
+(function () {
+  'use strict';
+  if (window.location.hash.length < 1) {
     return;
+  }
+
+  var fragmentLinks = {
+    /* WATTSI_INSERTS_FRAGMENT_LINKS_HERE */
+  };
 
   var fragid = window.location.hash.substr(1);
-  if (fragid && document.getElementById(fragid))
-    return;
 
-  var script = document.createElement('script');
-  script.src = '/multipage/fragment-links.js';
-  document.body.appendChild(script);
-});
+  if (fragid && document.getElementById(fragid)) {
+    return;
+  }
+
+  // handle section-foo.html links from the old old multipage version,
+  // and broken foo.html from the new version
+  if ((!fragid) || !(fragid in fragment_links)) {
+    var m = window.location.pathname.match(/\/(?:section-)?([\w\-]+)\.html/);
+    if (m) {
+      fragid = m[1];
+    }
+  }
+
+  var page = fragmentLinks[fragid];
+  if (page) {
+    window.location.replace(page + '.html#' + fragid);
+  }
+})();
