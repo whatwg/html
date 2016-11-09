f06953645ae80bf01b697b1014d0a647bb094ace
Simon Pieters <zcorpan@gmail.com>
2016-11-09 16:56:16 +0100

    Meta: Make link-fixup.js lightweight
    This file was about 2.8MB and was always run on multipage/ but
    the common case is to no-op. Instead XHR the fragment-links.json
    file only when necessary.
    
    Also fix 404.html to use the correct URL to link-fixup.js and use
    `defer`.
    
    Fixes #2026.

--- link-fixup.js
+++ link-fixup.js
@@ -4,27 +4,31 @@
     return;
   }
 
-  var fragmentLinks = {
-    /* WATTSI_INSERTS_FRAGMENT_LINKS_HERE */
-  };
-
   var fragid = window.location.hash.substr(1);
 
   if (fragid && document.getElementById(fragid)) {
     return;
   }
 
-  // handle section-foo.html links from the old old multipage version,
-  // and broken foo.html from the new version
-  if (!fragid || !(fragid in fragmentLinks)) {
-    var m = window.location.pathname.match(/\/(?:section-)?([\w\-]+)\.html/);
-    if (m) {
-      fragid = m[1];
-    }
-  }
+  var xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+  xhr.open('GET', '/multipage/fragment-links.json');
+  xhr.onload = function() {
+    var fragmentLinks = xhr.response;
 
-  var page = fragmentLinks[fragid];
-  if (page) {
-    window.location.replace(page + '.html#' + fragid);
-  }
+    // handle section-foo.html links from the old old multipage version,
+    // and broken foo.html from the new version
+    if (!fragid || !(fragid in fragmentLinks)) {
+      var m = window.location.pathname.match(/\/(?:section-)?([\w\-]+)\.html/);
+      if (m) {
+        fragid = m[1];
+      }
+    }
+
+    var page = fragmentLinks[fragid];
+    if (page) {
+      window.location.replace(page + '.html#' + fragid);
+    }
+  };
+  xhr.send();
 })();
